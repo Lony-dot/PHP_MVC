3d3bdc54a108c30a14a998c1946f1ee0f91ad5d3
Lony <loonydarck@hotmail.com>
2023-03-15 18:33:48 -0300

    Estilização da View e criação de novas funções

--- app/Controller/Pages/Home.php
+++ app/Controller/Pages/Home.php
@@ -4,17 +4,23 @@ namespace App\Controller\Pages;
 
 use \App\Utils\View;
 
-class Home{
+class Home extends Page{
 
     /**
      * Método responsável por retornar o conteúdo (view) da nossa home
      * @return string
      */
     public static function getHome(){
-        return View::render('pages/home',[
+        //VIEW DA HOME
+      $content =  View::render('pages/home',
+        [
             'name' => 'WDEV - Canal',
-            'description' => 'Canal do youtube: https://youtube.com.br/wdevoficial'
+            'description' => 'Canal do youtube: https://youtube.com.br/wdevoficial',
+            'site' => 'https://youtube.com.br/wdevoficial',
         ]);
+
+        //RETORNA A VIEW DA PÁGINA
+        return parent::getPage('WDEV - Canal - HOME', $content);
 
     }
 }
---
+++ app/Controller/Pages/Page.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Pages;
+
+use \App\Utils\View;
+
+class Page{
+    /**
+     * Método responsável por renderizar o topo da página
+     */
+    private static function getHeader()
+    {
+        return View::render('pages/header');
+    }
+
+    /**
+     * Método responsável por retornar o conteúdo (view) da nossa página  genérica.
+     * @return string
+     */
+    public static function getPage($title, $content){
+        return View::render('pages/page',[
+           'title' => $title,
+           'header' =>self::getHeader(),
+           'content' => $content
+        ]);
+
+    }
+}
