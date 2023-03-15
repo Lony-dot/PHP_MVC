e6d3d6545d117e6776561fe3bd05f71ba1c4691e
Lony <loonydarck@hotmail.com>
2023-03-15 18:46:32 -0300

    Models

---
+++ Model/Entity/Organization.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Model\Entity;
+
+class Organization
+{
+    /**
+     * ID da organização
+     * @var integer
+     */
+    public $id = 1;
+
+    /**
+     * Nome da organização
+     */
+    public $name = 'Canal WDEV';
+
+    /**
+     * Site da organização
+     * @var string
+     */
+    public $site = 'htpps://youtube.com/wdevoficial';
+
+    /**
+     * Descrição da organização
+     */
+    public $description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
+   
+}
