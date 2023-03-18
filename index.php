0132eb413bb66442bbc1eba5f84674e065f5c403
Lony <loonydarck@hotmail.com>
2023-03-18 13:26:00 -0300

    Implementando um gerenciador de rotas

--- index.php
+++ index.php
@@ -2,12 +2,21 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+use \App\Http\Router;
+use \App\Http\Response;
 use \App\Controller\Pages\Home;
 
-$obResponse = new \App\Http\Response(200, 'Olá mundo');
-$obResponse->sendResponse();
-/*echo "<pre>";
-print_r($obResponse);
-echo "</pre>";*/
-exit;
-echo Home::getHome();
+define('URL', 'http://localhost/mvc');
+
+$obRouter = new Router(URL);
+//ROTA HOME
+$obRouter->get('/',[
+    function(){
+        return new Response(200, Home::getHome());
+    }
+]);
+
+//IMPRIME O RESPONSE DA ROTA
+$obRouter->run()
+        ->sendResponse();
+
