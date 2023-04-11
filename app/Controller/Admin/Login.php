9fc47bc93a5ed663190a2434680e014ce08bc9d7
Lony <loonydarck@hotmail.com>
2023-04-11 20:42:49 -0300

    Autenticação de usuários no painel

--- app/Controller/Admin/Login.php
+++ app/Controller/Admin/Login.php
@@ -2,19 +2,30 @@
 
 namespace App\Controller\Admin;
 
-use App\Utils\View;
+use \App\Model\Entity\User;
+use \App\Utils\View;
+use \App\Session\Admin\Login as SessionAdminLogin;
 
 class Login extends Page
 {
     /**
      * Método responsável por retornar a renderização da página de login
      * @param Request $request
+     * @param string $errorMessage
      * @return string 
      */
-    public static function getLogin($request)
+    public static function getLogin($request, $errorMessage = null)
     {
+
+        //STATUS
+
+        $status = !is_null($errorMessage) ? view::render('admin/login/status',[
+            'mensagem' => $errorMessage
+        ]) : '';
         //CONTEÚDO DA PÁGINA DE LOGIN
-        $content = View::render('admin/login',[]);
+        $content = View::render('admin/login',[
+            'status' => $status
+        ]);
 
         //RETORNA A PÁGINA COMPLETA
         return parent::getPage('Login > WDEV', $content);
@@ -29,6 +40,40 @@ class Login extends Page
         //POST VARS
         $postVars = $request->getPostVars();
         $email    = $postVars['email'] ?? '';
-        $senha    = $postVars['senha'] ?? '';
+        $senha = $postVars['senha'] ?? '';
+
+        //BUSCA USUÁRIO PELO E-MAIL
+        $obUser = User::getUserByEmail($email);
+        if(!$obUser instanceof User)
+        {
+            return self::getLogin($request, 'E-mail ou senha inválidos');
+        }
+
+        //VERIFICA A SENHA DO USUÁRIO
+        if(!password_verify($senha, $obUser->senha)){
+            return self::getLogin($request, 'E-mail ou senha inválidos');
+        }
+
+         //CRIA A SESSÃO DE LOGIN
+        SessionAdminLogin::login($obUser);
+
+        //REDIRECIONA O USUÁRIO PARA A HOME DO ADMIN
+        $request->getRouter()->redirect('/admin');
     }
+
+    /**
+     * Método responsável por deslogar o usuário
+     * @param Request $request
+     */
+    public static function setLogout($request)
+    {
+        //DESTRÓI A SESSÃO DE LOGIN
+        SessionAdminLogin::logout();
+
+        //REDIRECIONA O USUÁRIO PARA A TELA DE LOGIN
+        $request->getRouter()->redirect('/admin/login');
+    }
+
+   
+
 }
---
+++ app/Http/Middleware/RequireAdminLogout.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use \App\Session\Admin\Login as SessionAdminLogin;
+
+class RequireAdminLogout
+{
+    /**
+     * Método responsável por executar as ações do middleware
+     * @param  Request $request
+     * @param  Closure $next
+     * @return Response
+     */
+    public function handle($request, $next)
+    {
+
+        //VERIFICA SE O USUÁRIO ESTÁ LOGADO
+        if(SessionAdminLogin::isLogged())
+        {
+            $request->getRouter()->redirect('/admin');
+        }
+        
+        //CONTINUA A EXECUÇÃO
+        return $next($request);
+    }
+}
